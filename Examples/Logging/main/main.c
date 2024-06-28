de987c07df80844255f9050c486b65bead59d187
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-06-29 03:43:05 +0530

    logging example project

---
+++ Examples/Logging/main/main.c
@@ -0,0 +1,29 @@
+/**
+ * @file main.c
+ * @author Omegaki113r
+ * @date Saturday, 29th June 2024 3:24:38 am
+ * @copyright Copyright 2024 - 2024 0m3g4ki113r, Xtronic
+ * */
+/*
+ * Project: Logging
+ * File Name: main.c
+ * File Created: Saturday, 29th June 2024 3:24:38 am
+ * Author: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Last Modified: Saturday, 29th June 2024 3:29:31 am
+ * Modified By: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Copyright 2024 - 2024 0m3g4ki113r, Xtronic
+ * -----
+ * HISTORY:
+ * Date      	By	Comments
+ * ----------	---	---------------------------------------------------------
+ */
+
+#include <stdio.h>
+
+#include "OmegaLoggingSystemController.h"
+
+void app_main(void)
+{
+}
