68768a3e0c2478d0a6383647b0c8a101d1859fbd
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-06-29 02:21:40 +0530

    initial commit for Logging

---
+++ OmegaLoggingSystemController/src/OmegaLoggingSystemController.c
@@ -0,0 +1,23 @@
+/**
+ * @file OmegaLoggingController.c
+ * @author Omegaki113r
+ * @date Saturday, 29th June 2024 1:54:40 am
+ * @copyright Copyright <<projectCreationYear>> - 2024 0m3g4ki113r, Xtronic
+ * */
+/*
+ * Project: src
+ * File Name: OmegaLoggingController.c
+ * File Created: Saturday, 29th June 2024 1:54:40 am
+ * Author: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Last Modified: Saturday, 29th June 2024 2:06:37 am
+ * Modified By: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Copyright <<projectCreationYear>> - 2024 0m3g4ki113r, Xtronic
+ * -----
+ * HISTORY:
+ * Date      	By	Comments
+ * ----------	---	---------------------------------------------------------
+ */
+
+#include "OmegaLoggingSystemController.h"
