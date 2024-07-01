87ac3d0ca72050f4949fd2430b4e5059299c3d30
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-07-01 21:16:11 +0530

    refactor

--- Examples/Logging/main/main.c
+++ Examples/Logging/main/main.c
@@ -10,7 +10,7 @@
  * File Created: Saturday, 29th June 2024 3:24:38 am
  * Author: Omegaki113r (omegaki113r@gmail.com)
  * -----
- * Last Modified: Saturday, 29th June 2024 6:24:48 pm
+ * Last Modified: Monday, 1st July 2024 1:01:09 pm
  * Modified By: Omegaki113r (omegaki113r@gmail.com)
  * -----
  * Copyright 2024 - 2024 0m3g4ki113r, Xtronic
@@ -36,28 +36,32 @@ void app_main(void)
     esp_vfs_spiffs_conf_t spiffs_config = {
         .base_path = "/storage",
         .max_files = 5,
+        // .partition_label = "storage",
         .format_if_mount_failed = false,
     };
     ESP_ERROR_CHECK(esp_vfs_spiffs_register(&spiffs_config));
     {
-        FileHandle handle = OmegaFileSystemController_open_file("/storage/log.h", eREADING | eWRITING | eAPPEND);
+        FileHandle handle = OmegaFileSystemController_open_file("/storage/log.txt", eREADING);
         const size_t sz = OmegaFileSystemController_get_file_size(handle);
         char *buf = malloc(sz + 1);
         if (buf == NULL)
             return;
-        OmegaFileSystemController_read_file(handle, eREAD_ALL, (uint8_t *)buf, sz);
+        size_t read_size = 0;
+        while ((read_size = OmegaFileSystemController_read_file(handle, eREAD_LINE, (uint8_t *)buf, sz)) > 0)
+        {
+            OMEGA_PLOGI("%s", buf);
+            vTaskDelay(pdMS_TO_TICKS(100));
+        }
         OmegaFileSystemController_close_file(handle);
-        OMEGA_PLOGV("%d %s", sz, buf);
         free(buf);
     }
     uint64_t i = 0;
-    vTaskDelay(pdMS_TO_TICKS(1000));
     for (;;)
     {
-        PROFILE_START();
+        PROFILE_START(__func__);
         OMEGA_PLOGI("%llu", i++);
-        OMEGA_FLOGI("hello world");
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        OMEGA_EVENTLOGI("hello world");
+        vTaskDelay(pdMS_TO_TICKS(100));
         PROFILE_END(__func__);
     }
 }
